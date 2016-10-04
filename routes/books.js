7f290b20ed4f9f14a514f48327da94e5a5958fc7
rsstdd <rosstod@gmail.com>
2016-10-04 12:54:15 -0700

    Established books GET route

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,25 @@
 'use strict';
 
 const express = require('express');
+const boom = require('boom');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (_req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .then((rows) => {
+      const books = camelizeKeys(rows);
+
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
