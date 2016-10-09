9a825176b2b3cee7389018cc2f39444e164ce6a7
rsstdd <rosstod@gmail.com>
2016-10-09 14:23:37 -0700

    Debugging /token GET

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,153 @@
 'use strict';
 
+const boom = require('boom');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+const authorize = function(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(boom.create(401, 'Unauthorized'));
+    }
+
+    req.token = decoded;
+
+    next();
+  });
+};
+
+router.get('/favorites', authorize, (req, res, next) => {
+  const { userId } = req.token;
+
+  knex('favorites')
+    .innerJoin('favorites', 'favorites.id', 'favorites.track_id')
+    .where('favorites.user_id', userId)
+    .orderBy('favorites.title', 'ASC')
+    .then((rows) => {
+      const favorites = camelizeKeys(rows);
+
+      res.send(favorites);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/favorites/:id', authorize, (req, res, next) => {
+  const { userId } = req.token;
+
+    knex('favorites')
+    .innerJoin('favorites', 'favorites.id', 'favorites.track_id')
+    .where('favorites.user_id', userId)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Not Found');
+      }
+
+      const track = camelizeKeys(row);
+
+      res.send(track);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/favorites/:id',  authorize, (req, res, next) => {
+  const { userId } = req.token;
+  const { title, artist, likes } = req.body;
+
+  if (!title || !title.trim()) {
+    return next(boom.create(400, 'Title must not be blank'));
+  }
+
+  if (!artist || !artist.trim()) {
+    return next(boom.create(400, 'Artist must not be blank'));
+  }
+
+  if (!Number.isInteger(likes)) {
+    return next(boom.create(400, 'Likes must be an integer'));
+  }
+
+  const insertTrack = { title, artist, likes };
+
+  knex('favorites')
+    .insert(decamelizeKeys(insertTrack), '*')
+    .then((rows) => {
+      const track = camelizeKeys(rows[0]);
+
+      res.send(track);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+//
+// router.patch('/favorites/:id',  authorize, (req, res, next) => {
+//   knex('favorites')
+//     .where('id', req.params.id)
+//     .first()
+//     .then((track) => {
+//       if (!track) {
+//         throw boom.create(404, 'Not Found');
+//       }
+//
+//       const { title, artist } = req.body;
+//       const updateTrack = {};
+//
+//       if (title) {
+//         updateTrack.title = title;
+//       }
+//
+//       if (artist) {
+//         updateTrack.artist = artist;
+//       }
+//
+//       return knex('favorites')
+//         .update(decamelizeKeys(updateTrack), '*')
+//         .where('id', req.params.id);
+//     })
+//     .then((rows) => {
+//       const track = camelizeKeys(rows[0]);
+//
+//       res.send(track);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
+//
+// router.delete('/favorites/:id',  authorize, (req, res, next) => {
+//   let track;
+//
+//   knex('favorites')
+//     .where('id', req.params.id)
+//     .first()
+//     .then((row) => {
+//       if (!row) {
+//         throw boom.create(404, 'Not Found');
+//       }
+//
+//       track = camelizeKeys(row);
+//
+//       return knex('favorites')
+//         .del()
+//         .where('id', req.params.id);
+//     })
+//     .then(() => {
+//       delete track.id;
+//
+//       res.send(track);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
 
 module.exports = router;
--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,69 @@
 'use strict';
 
+const boom = require('boom');
+const bcrypt = require('bcrypt-as-promised');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+const cookieParser = require('cookie-parser');
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.post('/token', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(400, 'Password must not be blank'));
+  }
+
+  let user;
+
+  knex('users') // in the users password
+    .where('email', email) // where 'email' matches email var
+    .first() // select the first obj from the arr.
+    .then((row) => { // then do something with that data
+      if (!row) { // if there is no match
+        throw boom.create(400, 'Bad email or password'); // you've got an error the password doesn't match. but you want to be vague
+      }
+
+      user = camelizeKeys(row); // since this is psql, make if JS
+
+      return bcrypt.compare(password, user.hashedPassword); // then return the comparison of the password var to the hashed/salted password of the db
+    })
+    .then(() => {
+      delete user.hashedPassword; // then delete it
+
+
+      const expiry = new Date(Date.now() + 1000 * 60 * 60 * 3); // 3 hours
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: '3h'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: expiry,
+        secure: router.get('env') === 'production'
+      });
+
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/token', (req, res, next) => {
+  res.clearCookie('token');
+  res.send(true);
+});
 
 module.exports = router;
